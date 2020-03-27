0898978c867b9421854263e676c56c964efb68bb
dhimasadip <dhimasadip.dap@gmail.com>
2020-03-27 13:09:27 +0700

    Exercise 4

--- Exercise 4.js
+++ Exercise 4.js
@@ -2,58 +2,65 @@ var tanggal = 1
 var bulan = 1
 var tahun = 1998
 
-switch(bulan) {
-	case 1:
-		bulan = 'Januari'
-		console.log(`${tanggal} ${bulan} ${tahun}`)
-		break
-	case 2:
-		bulan = 'Februari'
-		console.log(`${tanggal} ${bulan} ${tahun}`)
-		break
-	case 3:
-		bulan = 'Maret'
-		console.log(`${tanggal} ${bulan} ${tahun}`)
-		break
-	case 4:
-		bulan = 'April'
-		console.log(`${tanggal} ${bulan} ${tahun}`)
-		break
-	case 5:
-		bulan = 'Mei'
-		console.log(`${tanggal} ${bulan} ${tahun}`)
-		break
-	case 6:
-		bulan = 'Juni'
-		console.log(`${tanggal} ${bulan} ${tahun}`)
-		break
-	case 7:
-		bulan = 'Juli'
-		console.log(`${tanggal} ${bulan} ${tahun}`)
-		break
-	case 8:
-		bulan = 'Agustus'
-		console.log(`${tanggal} ${bulan} ${tahun}`)
-		break
-	case 9:
-		bulan = 'September'
-		console.log(`${tanggal} ${bulan} ${tahun}`)
-		break
-	case 10:
-		bulan = 'Oktober'
-		console.log(`${tanggal} ${bulan} ${tahun}`)
-		break
-	case 11:
-		bulan = 'November'
-		console.log(`${tanggal} ${bulan} ${tahun}`)
-		break
-	case 12:
-		bulan = 'Desember'
-		console.log(`${tanggal} ${bulan} ${tahun}`)
-		break
-	default:
-		console.log('input 12 >= bulan >= 1')
-		break
-}
 
+if (tanggal > 31 || tanggal < 1 || tahun > 2200 || tahun < 1900) {
+	console.log('input tanggal 1-31 dan tahun 1900-2200');
+	
+} else {
+		
+	switch(bulan) {
+		case 1:
+			bulan = 'Januari'
+			console.log(`${tanggal} ${bulan} ${tahun}`)
+			break
+		case 2:
+			bulan = 'Februari'
+			console.log(`${tanggal} ${bulan} ${tahun}`)
+			break
+		case 3:
+			bulan = 'Maret'
+			console.log(`${tanggal} ${bulan} ${tahun}`)
+			break
+		case 4:
+			bulan = 'April'
+			console.log(`${tanggal} ${bulan} ${tahun}`)
+			break
+		case 5:
+			bulan = 'Mei'
+			console.log(`${tanggal} ${bulan} ${tahun}`)
+			break
+		case 6:
+			bulan = 'Juni'
+			console.log(`${tanggal} ${bulan} ${tahun}`)
+			break
+		case 7:
+			bulan = 'Juli'
+			console.log(`${tanggal} ${bulan} ${tahun}`)
+			break
+		case 8:
+			bulan = 'Agustus'
+			console.log(`${tanggal} ${bulan} ${tahun}`)
+			break
+		case 9:
+			bulan = 'September'
+			console.log(`${tanggal} ${bulan} ${tahun}`)
+			break
+		case 10:
+			bulan = 'Oktober'
+			console.log(`${tanggal} ${bulan} ${tahun}`)
+			break
+		case 11:
+			bulan = 'November'
+			console.log(`${tanggal} ${bulan} ${tahun}`)
+			break
+		case 12:
+			bulan = 'Desember'
+			console.log(`${tanggal} ${bulan} ${tahun}`)
+			break
+		default:
+			console.log('input 12 >= bulan >= 1')
+			break
+	}
+
+}
 
